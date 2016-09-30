ac4bac9e1995c9301131c77f5eae08223d016bb2
Vlad Filippov <vlad.filippov@gmail.com>
2016-09-30 14:18:12 -0400

    feat(devices): enable devices for sampled users (#42) r=vbudhram

--- clients/devices.js
+++ clients/devices.js
@@ -3,11 +3,13 @@
 module.exports = {
   name: 'Is the devices list settings section visible',
   startDate: '2015-01-01',
-  subjectAttributes: ['forceDeviceList'],
+  subjectAttributes: ['forceDeviceList', 'isMetricsEnabledValue'],
   independentVariables: ['deviceListVisible'],
   eligibilityFunction: function (subject) {
-    if (subject && subject.forceDeviceList) {
-      return true;
+    if (subject) {
+      if (subject.isMetricsEnabledValue || subject.forceDeviceList) {
+        return true;
+      }
     }
 
     return false;
