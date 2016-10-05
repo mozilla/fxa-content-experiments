caf380d5439a5f19042c41ff5dcf352b81217799
Vlad Filippov <vlad.filippov@gmail.com>
2016-10-05 19:34:26 -0400

    feat(devices): add uid based roll out instead of metrics uuid (#43) r=rfk

--- clients/devices.js
+++ clients/devices.js
@@ -3,12 +3,18 @@
 module.exports = {
   name: 'Is the devices list settings section visible',
   startDate: '2015-01-01',
-  subjectAttributes: ['forceDeviceList', 'isMetricsEnabledValue'],
+  subjectAttributes: ['forceDeviceList', 'uid'],
   independentVariables: ['deviceListVisible'],
   eligibilityFunction: function (subject) {
+    var sampleRate = 0.1;
+
     if (subject) {
-      if (subject.isMetricsEnabledValue || subject.forceDeviceList) {
+      if (subject.forceDeviceList) {
         return true;
+      }
+
+      if (subject.uid) {
+        return !! (this.bernoulliTrial(sampleRate, subject.uid));
       }
     }
 
