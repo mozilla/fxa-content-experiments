1d292f81de8965c1ed102b2993da496b7b69179c
Shane Tomlinson <stomlinson@mozilla.com>
2017-01-26 11:45:22 +0000

    Merge pull request #52 from mozilla/connectAnotherDevice-to-25-percent r=self, vladikoff
    Remove connectAnotherDevice from the `verification` experiments. Place
    50% of users into the `connectAnotherDevice` experiment, ignoring `isMetricsEnabledValue`. 25% of users will go into each of the two buckets, compared to 1.25% now.
    
    I'm self-merging after input from vladikoff w/o further review. I'll take responsibility for any blowups.

--- connect-another-device/connect-another-device.js
+++ connect-another-device/connect-another-device.js
@@ -4,16 +4,25 @@ module.exports = {
   name: 'Should the user see the "connect another device" screen',
   hypothesis: 'A nudge to connect another device will help increase multi-device users',
   startDate: '2015-01-01',
-  subjectAttributes: ['able', 'uniqueUserId', 'isMetricsEnabledValue', 'forceExperimentGroup'],
+  subjectAttributes: ['uniqueUserId', 'forceExperiment', 'forceExperimentGroup'],
   independentVariables: ['connectAnotherDevice'],
   eligibilityFunction: function (subject) {
-    if (subject && subject.able) {
-      return subject.able.choose('chooseAbExperiment', subject) === 'connectAnotherDevice';
-
+    if (! subject) {
+      return false;
     }
 
-    return false;
+    if (subject.forceExperiment === 'connectAnotherDevice') {
+      return true;
+    }
+
+    // Place 50% of users into the two buckets, meaning the feature
+    // will be enabled for ~25% of users. 'isMetricsEnabledValue' is
+    // ignored, we have sufficiently high numbers that we'll assume
+    // an equal number of users who have metrics enabled are in each
+    // bucket. This assumption is measured in DataDog.
+    return this.bernoulliTrial(0.5, subject.uniqueUserId);
   },
+
   groupingFunction: function (subject) {
     var GROUPS = ['control', 'treatment'];
     var choice = this.uniformChoice(GROUPS, subject.uniqueUserId);
