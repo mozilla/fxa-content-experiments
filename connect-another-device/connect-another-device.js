342f0ed278ad4398b6e6ab1976ff9876e105621f
Shane Tomlinson <stomlinson@mozilla.com>
2017-02-27 12:10:32 +0000

    feat(CAD): Enable CAD for 100% of users.

--- connect-another-device/connect-another-device.js
+++ connect-another-device/connect-another-device.js
@@ -4,35 +4,15 @@ module.exports = {
   name: 'Should the user see the "connect another device" screen',
   hypothesis: 'A nudge to connect another device will help increase multi-device users',
   startDate: '2015-01-01',
-  subjectAttributes: ['uniqueUserId', 'forceExperiment', 'forceExperimentGroup'],
+  subjectAttributes: [],
   independentVariables: ['connectAnotherDevice'],
   eligibilityFunction: function (subject) {
-    if (! subject) {
-      return false;
-    }
-
-    if (subject.forceExperiment === 'connectAnotherDevice') {
-      return true;
-    }
-
-    // Place 50% of users into the two buckets, meaning the feature
-    // will be enabled for ~25% of users. 'isMetricsEnabledValue' is
-    // ignored, we have sufficiently high numbers that we'll assume
-    // an equal number of users who have metrics enabled are in each
-    // bucket. This assumption is measured in DataDog.
-    return this.bernoulliTrial(0.5, subject.uniqueUserId);
+    return true;
   },
 
   groupingFunction: function (subject) {
-    var GROUPS = ['control', 'treatment'];
-    var choice = this.uniformChoice(GROUPS, subject.uniqueUserId);
-
-    if (subject.forceExperimentGroup) {
-      choice = subject.forceExperimentGroup;
-    }
-
     return {
-      connectAnotherDevice: choice
+      connectAnotherDevice: 'treatment'
     };
   }
 };
