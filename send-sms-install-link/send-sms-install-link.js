701bb0512e0c8075272a0f1dbfc2cf5e857e397c
Shane Tomlinson <stomlinson@mozilla.com>
2017-04-24 15:20:32 +0100

    feat(sms): Bump SMS experiment to 20%, 10% in each bucket. (#63)  r=vladikoff

--- send-sms-install-link/send-sms-install-link.js
+++ send-sms-install-link/send-sms-install-link.js
@@ -23,9 +23,40 @@ module.exports = {
       return true;
     }
 
-    // enable for everyone who reports metrics to DataDog - 10% of the population.
-    // Of these, half will be in the control group, half in the treatment group.
-    return !! subject.isMetricsEnabledValue;
+    // enable for everyone who reports metrics to DataDog - 10% of the population
+    // + 10 extra percent of the population who are not reporting to DataDog, for
+    // a total of 20%.
+    //
+    // Getting 20% total percent when 10% is already chosen and selections
+    // are independent is a bit strange. We start with the 10% that report
+    // to DataDog, then add another 10%. Group A is the group who report to
+    // DataDog, Group B is the 2nd selection. 10% of Group B will be a
+    // part of Group A, so we have to ignore them and choose others in
+    // their place. This means we have to choose > 10% of the general
+    // population for Group B to arrive at 10% of the population
+    // who are not already in Group A.
+    if (subject.isMetricsEnabledValue) {
+      return true;
+    }
+
+    // Enable for an additional 11.1111% of the population.
+    //
+    // N = total population
+    // x = % to solve for.
+    //
+    // .2N = .1N + (xN - .1xN)
+    // /\    /\     /\   /\
+    // |     |      |    |
+    // |     |      |     --- Group B also in Group A
+    // |     |      --------- Group B
+    // |     ---------------- Group A (people reporting to DataDog)
+    // ---------------------- target
+    //
+    // .2N = .1N + .9xN
+    // .1N = .9xN
+    // .1N / .9N = x
+    // .11111 = x
+    return this.bernoulliTrial(0.1111, subject.uniqueUserId);
   },
 
   groupingFunction: function (subject) {
